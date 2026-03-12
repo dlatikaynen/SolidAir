c38efb790208a456d2cea1e908314a65872e4901
dlatikay <latikaynen@loidl-consulting.at>
2026-03-12 23:55:58 +0100

    # - menu

--- SolidAir/Resource.h
+++ SolidAir/Resource.h
@@ -1,30 +1,69 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by SolidAir.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_SOLIDAIR_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_SOLIDAIR			107
-#define IDI_SMALL				108
-#define IDC_SOLIDAIR			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// Next default values for new objects
 //
+#define IDC_MYICON                      2
+#define IDD_SOLIDAIR_DIALOG             102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_SOLIDAIR                    107
+#define IDI_SMALL                       108
+#define IDC_SOLIDAIR                    109
+#define IDR_MAINFRAME                   128
+#define ID_GAME_NEW                     32771
+#define ID_Menu                         32772
+#define ID_GAME_SAVE                    32773
+#define ID_GAME_SAVEAS                  32774
+#define ID_EXIT_REALLY                  32775
+#define ID_REALLY_YES                   32776
+#define ID_REALLY_NO                    32777
+#define ID_REALLY_MAYBE                 32778
+#define ID_YES_WHY                      32779
+#define ID_WHY_THISISNOTAMENUITEM       32780
+#define ID_LANGUAGE_ENGLISH             32781
+#define ID_LANGUAGE_DEUTSCH             32782
+#define ID_LANGUAGE_SUOMI               32783
+#define ID_LANGUAGE_32784               32784
+#define ID_HELP_ONLINEDOCS              32785
+#define ID_HELP_QUICKREFERENCE          32786
+#define ID_SOUND_SOUNDEFFECTS           32787
+#define ID_SOUND_BACKGROUNDMUSIC        32788
+#define ID_SETTINGS_CARDBACKGROUND      32789
+#define ID_SETTINGS_BACKGROUNDCOLOR     32790
+#define ID_BACKGROUNDCOLOR_GREEN        32791
+#define ID_BACKGROUNDCOLOR_BLACK        32792
+#define ID_BACKGROUNDCOLOR_DARKGRAY     32793
+#define ID_BACKGROUNDCOLOR_DARKBLUE     32794
+#define ID_BACKGROUNDCOLOR_PINK         32795
+#define ID_CARDBACK_BLUEDOT             32796
+#define ID_CARDBACK_FELINE              32797
+#define ID_CARDBACK_COLORFUL            32798
+#define ID_CARDBACK_CUBERT              32799
+#define ID_CARDBACK_DIVINE              32800
+#define ID_CARDBACK_ICE                 32801
+#define ID_CARDBACK_MAPLE               32802
+#define ID_CARDBACK_PAPER               32803
+#define ID_CARDBACK_PARKETT             32804
+#define ID_CARDBACK_PINE                32805
+#define ID_CARDBACK_RED                 32806
+#define ID_CARDBACK_ROCKS               32807
+#define ID_CARDBACK_SAFETY              32808
+#define ID_CARDBACK_SKY                 32809
+#define ID_CARDBACK_SPACE               32810
+#define ID_SETTINGS_SOUNDFX             32811
+#define ID_SETTINGS_BACKGROUNDMUSIC     32812
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32813
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif

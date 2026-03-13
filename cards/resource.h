93dc522336f28b64cf8224f286fbd52d1ffa285f
dlatikay <latikaynen@loidl-consulting.at>
2026-03-13 09:01:51 +0100

    # - all cards defined

--- cards/resource.h
+++ cards/resource.h
@@ -34,12 +34,52 @@
 #define laub_bublikum                   129
 #define IDB_PNG2                        130
 #define joker                           130
+#define laub_3                          131
+#define herz_3                          132
+#define herz_4                          133
+#define herz_6                          134
+#define herz_7                          135
+#define herz_8                          136
+#define herz_9                          137
+#define herz_10                         138
+#define herz_asslikum                   139
+#define herz_bublikum                   140
+#define herz_kinigl                     141
+#define karo_2                          142
+#define karo_3                          143
+#define karo_4                          144
+#define karo_5                          145
+#define karo_6                          146
+#define karo_7                          147
+#define karo_8                          148
+#define karo_9                          149
+#define karo_asslikum                   150
+#define karo_bublikum                   151
+#define IDB_PNG25                       152
+#define karo_kinigl                     152
+#define laub_2                          153
+#define laub_4                          154
+#define laub_5                          155
+#define laub_6                          156
+#define laub_7                          157
+#define laub_8                          158
+#define laub_9                          159
+#define laub_10                         160
+#define pik_2                           161
+#define pik_3                           162
+#define pik_4                           163
+#define pik_5                           164
+#define pik_6                           165
+#define pik_7                           166
+#define pik_10                          167
+#define pik_asslikum                    168
+#define pik_kinigl                      169
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        172
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
